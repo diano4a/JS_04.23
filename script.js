00a7f3ab714dd4693571942439ec0aafcbfa281b
Diana <kdv012025@gmail.com>
2023-04-05 01:54:01 +0400

    [1.2:05.04]: finished second task

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use stict';
 
-// task 1
+//task 1
 
 let value = prompt('Введите число');
 
@@ -31,3 +31,28 @@ console.log(`Square: ${num ** 2}`);
 console.log(`isPrime: ${isPrime}`);
 console.log(`isEven: ${num % 2 === 0}`);
 console.log(`Delimiters: ${delimiters.join(', ')}`);
+
+// task 2
+
+let firstInput = prompt('Введите любой набор до трех символов включительно');
+while (firstInput === null || firstInput.trim().length === 0 || firstInput.trim().length > 3) {
+  console.log('Incorrect input!');
+  firstInput = prompt('Введите любой набор до трех символов включительно');
+}
+
+let secondInput = parseFloat(prompt('Введите любое число не меньше 1 и не больше 10'));
+while (isNaN(secondInput) || secondInput <= 0 || secondInput >= 10) {
+  console.log('Incorrect input!');
+  secondInput = parseFloat(prompt('Введите любое число не меньше 1 и не больше 10'));
+}
+
+let charArray = new Array(secondInput).fill(firstInput);
+let charString = charArray.join(' ');
+
+let gridString = '';
+
+for (let i = 0; i < secondInput; i++) {
+  gridString += `${charString}\n`;
+}
+
+console.log(gridString);
