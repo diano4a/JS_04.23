945976741a44e7efdaa6927d4b7a68e0d9338331
Diana <kdv012025@gmail.com>
2023-04-15 10:45:31 +0400

    [4.2:15.04]: finished second task

--- index.js
+++ index.js
@@ -14,3 +14,51 @@ function curry(func) {
   };
 
 }
+
+class Calculator {
+  constructor(X, Y) {
+    if (arguments.length !== 2|| typeof X !== 'number' || typeof Y !== 'number' || isNaN(X) || isNaN(Y)) {
+      throw new Error('');
+    }
+    this.X = X;
+    this.Y = Y;
+    this.getSum = this.getSum.bind(this);
+    this.getMul = this.getMul.bind(this);
+    this.getSub = this.getSub.bind(this);
+    this.getDiv = this.getDiv.bind(this);
+  }
+
+  setX(num) {
+    if (typeof num !== 'number' || isNaN(num)) {
+      throw new Error('');
+    }
+    this.X = num;
+  }
+
+  setY(num) {
+    if (typeof num !== 'number' || isNaN(num)) {
+      throw new Error('');
+    }
+    this.Y = num;
+  }
+
+  getSum() {
+    return this.X + this.Y;
+  }
+
+  getMul() {
+    return this.X * this.Y;
+  }
+
+  getSub() {
+    return Math.abs(this.X - this.Y);
+  }
+
+  getDiv() {
+    if (this.Y === 0) {
+      throw new Error('');
+    }
+    
+    return this.X / this.Y;
+  }
+}
